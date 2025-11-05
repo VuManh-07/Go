09ef88e6b61b32dd85d9702767f8ef6ace22cb4c
ManhVu-07 <manhvd1507@gmail.com>
2025-11-05 19:05:14 +0700

    learn: Non-blocking với timeout

--- Learn/Lesson_35/main.go
+++ Learn/Lesson_35/main.go
@@ -1,33 +1,55 @@
 // Non-Blocking Channel Operations
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+// func main() {
+// 	messages := make(chan string)
+// 	signals := make(chan bool)
+
+// 	select {
+// 	case msg := <-messages:
+// 		fmt.Println("received message ", msg)
+// 	default:
+// 		fmt.Println("no message received")
+// 	}
+
+// 	msg := "hi"
+// 	select {
+// 	case messages <- msg:
+// 		fmt.Println("Sent message: ", msg)
+// 	default:
+// 		fmt.Println("no message sent")
+// 	}
+
+// 	select {
+// 	case msg := <-messages:
+// 		fmt.Println("received message: ", msg)
+// 	case sig := <-signals:
+// 		fmt.Println("received signal: ", sig)
+// 	default:
+// 		fmt.Println("no active")
+// 	}
+// }
 
 func main() {
-	messages := make(chan string)
-	signals := make(chan bool)
+	ch := make(chan string, 1)
 
-	select {
-	case msg := <-messages:
-		fmt.Println("received message ", msg)
-	default:
-		fmt.Println("no message received")
-	}
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch <- "res"
+	}()
 
-	msg := "hi"
-	select {
-	case messages <- msg:
-		fmt.Println("Sent message: ", msg)
-	default:
-		fmt.Println("no message sent")
-	}
-
-	select {
-	case msg := <-messages:
-		fmt.Println("received message: ", msg)
-	case sig := <-signals:
-		fmt.Println("received signal: ", sig)
-	default:
-		fmt.Println("no active")
+	for {
+		select {
+		case msg := <-ch:
+			fmt.Println("msg: ", msg)
+			return
+		case <-time.After(500 * time.Millisecond):
+			fmt.Println("No res yet, doing something else...")
+		}
 	}
 }

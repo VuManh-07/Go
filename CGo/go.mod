253980abe2dd70e0a80f0d941cecc5cb7913ec5c
ManhVu-07 <manhvd1507@gmail.com>
2025-11-06 22:48:03 +0700

    CGo

---
+++ CGo/go.mod
@@ -0,0 +1,3 @@
+module github.com/ManhVu-07/Go/CGo
+
+go 1.25.3
---
+++ CGo/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "C" // Kích hoạt CGo
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world from Go")
+}

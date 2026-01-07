6d6c444e4c04af1a70d026b5bf837cb3467b9777
ManhVu-07 <manhvd1507@gmail.com>
2026-01-07 11:13:34 +0700

    feat: create global package for configuration and logger initialization

---
+++ Projects/go-ecommer-be-api/global/global.go
@@ -0,0 +1,11 @@
+package global
+
+import (
+	"github.com/VuManh-07/Go/Projects/go-ecommer-be-api/pkg/logger"
+	"github.com/VuManh-07/Go/Projects/go-ecommer-be-api/pkg/setting"
+)
+
+var (
+	Configs setting.Config
+	Logger  *logger.LoggerZap
+)
